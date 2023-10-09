c2adc454bf1614ad198f280d4b8819984e8b3e04
Hitesh1090 <hiteshrookie1090@gmail.com>
2023-10-09 16:44:03 +0530

    ggg

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = "shuttl-server.onrender.com:10000"
+const SOCKET_URL = "https://shuttl-server.onrender.com:10000"
 const socket = io(SOCKET_URL);
 export default socket;
