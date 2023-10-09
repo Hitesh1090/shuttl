af1a37644bc340b000f37ae2e47eb2612b8452fc
Hitesh1090 <hiteshrookie1090@gmail.com>
2023-10-09 16:38:35 +0530

    bruv

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = process.env.SERVER_URL || "http://localhost:8080"; //put {ip_addr}:port instead of localhost:8080 if u want to see in mobile also | port is the one in backend/index.js
+const SOCKET_URL = "shuttl-server.onrender.com:10000"
 const socket = io(SOCKET_URL);
 export default socket;
