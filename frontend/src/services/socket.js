b21f825adf836aa29891bc2fbd15c5bb807fcbbe
Hitesh1090 <hiteshrookie1090@gmail.com>
2023-10-09 21:46:14 +0530

    deleted:    frontend/.env

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-
-const SOCKET_URL = "https://shuttl-server.onrender.com:10000"
+// const SOCKET_URL = process.env.SERVER_URL || "http://localhost:8080";
+const SOCKET_URL = "https://shuttl-server.onrender.com";
 const socket = io(SOCKET_URL);
 export default socket;
