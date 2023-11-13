9654acd327b72814b392d64f23bdd55c66a7bf8b
Hitesh1090 <hiteshrookie1090@gmail.com>
2023-11-13 13:36:57 +0530

    img cen

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -56,10 +56,10 @@ const Home = () => {
       </nav>
 
       {/* Bootstrap Hero Section */}
-      <div className="container">
+      <div className="container py-5">
       <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div class="col-10 col-sm-8 col-lg-6">
-        <img src={heroImage} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="100" height="100" loading="lazy"/>
+        <img src={heroImage} class="d-block mx-sm-auto mx-md-auto mx-lg-auto img-fluid" alt="Bootstrap Themes" width="100" height="100" loading="lazy"/>
       </div>
       <div class="col-lg-6">
         <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Tired of waiting around ?</h1>
