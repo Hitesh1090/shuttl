c9be093367ce5e92a2f64c487cc240859411b64d
Hitesh1090 <hiteshrookie1090@gmail.com>
2024-04-25 04:12:50 +0530

    hopefully this works gps module

--- frontend/src/pages/Driver.js
+++ frontend/src/pages/Driver.js
@@ -27,10 +27,38 @@ function Driver() {
     }
   };
 
+  const fetchDataFromThingSpeak = async () => {
+  try {
+    const response = await fetch("https://api.thingspeak.com/channels/2521937/feeds.json?results=1");
+    const data = await response.json();
+
+    // Check if the response contains feeds array
+    if (data.feeds && data.feeds.length > 0) {
+      const feed = data.feeds[0]; // Get the first feed object
+      const latitude = parseFloat(feed.field1); // Extract latitude value and convert to float
+      const longitude = parseFloat(feed.field2); // Extract longitude value and convert to float
+
+      // Do something with latitude and longitude values
+      console.log("Latitude:", latitude);
+      console.log("Longitude:", longitude);
+      setCoordinates({ latitude, longitude });
+      setDriverType("MensHostel");
+      socket.emit("message", { latitude, longitude, driverType });
+    } else {
+      console.error("No feeds found in the response.");
+    }
+  } catch (error) {
+    console.error("Error fetching data from ThingSpeak:", error);
+  }
+};
+
+  
+
   useEffect(() => {
     fetchAndSendLocation();
     const interval = setInterval(() => {
       fetchAndSendLocation();
+      fetchDataFromThingSpeak();
     }, 5000);
 
     return () => {
