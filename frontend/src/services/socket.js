0ed51c760ca45665e9c7aad118876964c310b971
Hitesh1090 <hiteshrookie1090@gmail.com>
2024-04-28 15:07:08 +0530

    new1

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
-const SOCKET_URL = process.env.SERVER_URL || "http://localhost:8080";
+
+const SOCKET_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:8080";
 // const SOCKET_URL = "https://shuttl-server.onrender.com";
 const socket = io(SOCKET_URL);
 export default socket;
